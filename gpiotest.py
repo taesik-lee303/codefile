4c773fafc7abd8b67056c623441ee2ce136ea8ad
taesik-lee303 <lee200303@gmail.com>
2025-06-10 21:27:19 +0900

    Update gpiotest.py

--- gpiotest.py
+++ gpiotest.py
@@ -1,43 +1,46 @@
 #!/usr/bin/env python3
-from gpiozero import OutputDevice
+import lgpio
 import spidev
 import time
 
-# GPIO 설정 (gpiozero 사용)
-dc_pin = OutputDevice(24)
-reset_pin = OutputDevice(25)
+# GPIO 핸들 열기
+h = lgpio.gpiochip_open(0)
+
+# GPIO 핀 번호
+DC_PIN = 24
+RESET_PIN = 25
+
+# GPIO 출력으로 설정
+lgpio.gpio_claim_output(h, DC_PIN)
+lgpio.gpio_claim_output(h, RESET_PIN)
 
 # SPI 설정
 spi = spidev.SpiDev()
 spi.open(0, 0)
 spi.max_speed_hz = 32000000
-spi.mode = 0
 
 # 리셋
-reset_pin.off()
+lgpio.gpio_write(h, RESET_PIN, 0)
 time.sleep(0.1)
-reset_pin.on()
+lgpio.gpio_write(h, RESET_PIN, 1)
 time.sleep(0.1)
 
-# 명령/데이터 전송 함수
 def send_command(cmd):
-    dc_pin.off()  # Command mode
+    lgpio.gpio_write(h, DC_PIN, 0)
     spi.writebytes([cmd])
 
 def send_data(data):
-    dc_pin.on()  # Data mode
+    lgpio.gpio_write(h, DC_PIN, 1)
     if isinstance(data, list):
         spi.writebytes(data)
     else:
         spi.writebytes([data])
 
-# ILI9341 초기화
-send_command(0x01)  # Software Reset
+# 테스트
+send_command(0x01)
 time.sleep(0.2)
-
-print("Display initialized with gpiozero!")
+print("Display initialized with lgpio!")
 
 # 정리
 spi.close()
-dc_pin.close()
-reset_pin.close()
+lgpio.gpiochip_close(h)
